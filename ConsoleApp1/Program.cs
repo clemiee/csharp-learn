b0c09c0352a550aa6395889f6ace764954e1afa0
GNZTMPZ <27568445+clemiee@users.noreply.github.com>
2022-06-26 00:18:44 +0700

    data types and variables

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,31 @@ namespace learn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            /*
+            int x;
+            int y;
+
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("What is your name?");
+            Console.Write("Nama Awal: ");
+            string firstName;
+            firstName = Console.ReadLine();
+
+            //string last;
+            //Console.Write("Nama Akhir: ");
+            //last = Console.ReadLine();
+
+            Console.Write("Nama Akhir: ");
+            string myLastName = myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + firstName + " " + myLastName);
             Console.ReadLine();
         }
     }
